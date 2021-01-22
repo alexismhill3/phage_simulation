1769e31bdb384c2e00a6b1d5fd2651bfca75b762
Alexis Hill <alexismhill3@gmail.com>
2021-01-22 14:51:21 -0500

    move parameter optimization folder out of main repo

--- src/python/parameter-optimization/main.py
+++
@@ -1,8 +0,0 @@
-from scipy.optimize import differential_evolution
-import minimize
-import sys
-
-model_config, diff_evolution_config = sys.argv
-bounds = diff_evolution_config["bounds"]
-result = differential_evolution(minimize.run_simulation, bounds, args=(model_config, diff_evolution_config))
-print(result.x)
